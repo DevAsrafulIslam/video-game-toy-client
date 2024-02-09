044ae6cc05eb5ae6b63800ecc3454654c35a7708
MD Asraful Islam <asrafulislam.dev@gmail.com>
2024-02-09 19:08:55 +0600

    fix problem

--- src/pages/Home/VideoGames/GameCart.jsx
+++ src/pages/Home/VideoGames/GameCart.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const GameCart = ({ product }) => {
   const { name, id, title, image, price, rating } = product;
   return (
-    <div className="border hover:relative">
-      <button className="absolute text-white bg-sky-500 px-4 py-2 rounded-lg top-[50%] left-[35%]">
+    <div className="border ">
+      {/* <button className="absolute text-white bg-sky-500 px-4 py-2 rounded-lg top-[50%] left-[35%]">
         Buy Now
-      </button>
+      </button> */}
       <img className="w-full  mx-auto" src={image} alt="" />
       <div className="flex text-start items-center justify-around">
         {name}
